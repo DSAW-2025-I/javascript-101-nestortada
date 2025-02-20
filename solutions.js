85d03d41160d447117b852495d63e02e2f8746d6
nestortada <nestorandres4@hotmail.com>
2025-02-19 23:17:09 -0500

    Ejercicios

--- solutions.js
+++ solutions.js
@@ -1,26 +1,56 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  if (typeof a !== "number" || typeof b !== "number") {
+    console.log("Los Numeros de a y b  no son adecuados");
+    return;
+  }
+  return a + b;
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  if (typeof n !== "number" || n < 0) {
+    console.log("El numero N es adecuado");
+    return;
+  }
+  if (n === 0) return 1;
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  if (!Array.isArray(arr) || arr.some(isNaN)) {
+    console.log("El array no es el adecaudo");
+    return;
+  }
+  return Math.max(...arr);
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  if (typeof str !== "string") {
+    console.log("El string no es adecuado");
+    return;
+  }
+  const vowels = str.match(/[aeiou]/gi);
+  return vowels ? vowels.length : 0;
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  if (typeof n !== "number") {
+    console.log("El numero N no es adecuado");
+    return;
+  }
+  if (n < 2) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 module.exports = {
